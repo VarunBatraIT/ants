d5f69ec912a34a9cd40da62c1761d778292d39d6
Varun Batra <codevarun@gmail.com>
2016-11-30 16:39:21 +0530

    quick

--- queen.go
+++ queen.go
@@ -19,5 +19,29 @@ func scheduler() {
 		log.Println(location)
 		c = cron.NewWithLocation(location)
 	}
+	// Post self marketing!
+	c.AddFunc("0 30 8 * * 1,6", func(){
+		PostSelfMarketingOnLinkedin()
+	})
+	// Post jobs!
+	c.AddFunc("0 30 8 * * 1,6", func(){
+		PostJobOnLinkedin()
+	})
+	c.AddFunc("0 30 9,19 * * *", func(){
+		PostBlogOnLinkedin("leadership")
+		PostBlogOnTwitter("leadership")
+	})
+	c.AddFunc("0 35 9,19 * * *", func(){
+		PostBlogOnLinkedin("leadership")
+		PostBlogOnTwitter("leadership")
+	})
+	c.AddFunc("0 45 9,19 * * *", func(){
+		PostBlogOnLinkedin("marketing")
+		PostBlogOnTwitter("marketing")
+	})
+	c.AddFunc("0 0 10,20 * * *", func(){
+		PostBlogOnLinkedin("technical")
+		PostBlogOnTwitter("technical")
+	})
 	c.Start()
 }
